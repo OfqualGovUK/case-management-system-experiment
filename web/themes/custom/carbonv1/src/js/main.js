f9e039187bac50ad8f4adb98303a9cf110c27dc0
DDEV User <nobody@example.com>
2025-09-16 16:42:03 +0100

    Inline carbonv1 theme (snapshot of former submodule)

---
+++ web/themes/custom/carbonv1/src/js/main.js
@@ -0,0 +1,14 @@
+import '../scss/main.scss';
+// (Optional) add your own JS here
+console.log('carbonv1 theme loaded');
+// Accordion
+import '@carbon/web-components/es/components/accordion/index.js';
+
+// Date picker
+import '@carbon/web-components/es/components/date-picker/index.js';
+
+// Data table (core pieces)
+import '@carbon/web-components/es/components/data-table/index.js';
+
+// (Optional helpers)
+import '@carbon/web-components/es/components/pagination/index.js';
